79b89c4d354dcb87827f3a7ae11bb24c56c925cd
Conan Olivier <malina.anaho@gmail.com>
2017-01-13 15:04:37 +0100

    added Calendar / refactor classes to be done

--- app/src/main/java/com/malenea/todolist/MainActivity.java
+++ app/src/main/java/com/malenea/todolist/MainActivity.java
@@ -1,11 +1,14 @@
 package com.malenea.todolist;
 
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.content.res.AssetManager;
 import android.graphics.Typeface;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -18,6 +21,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
@@ -28,9 +32,11 @@ import android.widget.Toast;
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
 
+import static android.R.attr.datePickerDialogTheme;
 import static android.R.attr.typeface;
 
 
@@ -64,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
 
     // To show the des popup of a task
     public void showPopup(final View view) {
+
         LayoutInflater layoutInflater = LayoutInflater.from(this);
         final View promptView = layoutInflater.inflate(R.layout.popup, null);
 
@@ -78,6 +85,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        ImageButton btnCal = (ImageButton) promptView.findViewById(R.id.popup_cal);
+        btnCal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePicker();
+            }
+        });
+
         ImageButton btnBack = (ImageButton) promptView.findViewById(R.id.popup_back);
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -88,6 +103,33 @@ public class MainActivity extends AppCompatActivity {
 
         dialog.setView(promptView);
         dialog.show();
+    }
+
+    // Show Calendar
+
+    private void showDatePicker() {
+
+        final TextView txtDate = (TextView) findViewById(R.id.popup_txtDate);
+
+        DatePickerDialog dateDialog;
+
+        final Calendar c = Calendar.getInstance();
+        int mYear = c.get(Calendar.YEAR);
+        int mMonth = c.get(Calendar.MONTH);
+        int mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        dateDialog = new DatePickerDialog(MainActivity.this,
+                new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view,
+                                          int year, int monthOfYear, int dayOfMonth) {
+                        //txtDate.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                    }
+
+                }, mYear, mMonth, mDay);
+
+        dateDialog.show();
     }
 
     // Use adapter to populate db and list
